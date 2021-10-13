0c1abbfa2992a7d1e83ae690e153e1f2f273f216
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-10-13 16:25:20 +0200

    init commit

---
+++ src/test/groovy/com/bookshop/bookshop/controller/CopyControllerSpecification.groovy
@@ -0,0 +1,8 @@
+package com.bookshop.bookshop.controller
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class CopyControllerSpecification extends Specification {
+}
